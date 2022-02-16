c6c3012d0edb049d383642391d6f740eb9731f4c
Илья Пащук <illyadeveloper@gmail.com>
2022-02-16 21:11:18 +0200

    Fixed srcset

--- src/app/components/picture-wrap/picture-wrap.component.tsx
+++ src/app/components/picture-wrap/picture-wrap.component.tsx
@@ -3,8 +3,8 @@ import './picture-wrap.component.scss'
 
 interface PictureWrapProps {
     children?: React.ReactNode;
-    src?: string,
-    srcset?: string,
+    src?: any,
+    srcset?: any,
     hostClass?: string,
 }
 
@@ -13,8 +13,9 @@ class PictureWrapComponent extends Component<PictureWrapProps> {
         return (
             <article className={'picture-wrap'}>
                 <picture className={'picture-wrap__pic'}
-                         srcset={this.props.srcset || this.props.src}
-                         media='min-width: 1100px'>
+               >
+                    <source srcSet={this.props.srcset || this.props.src}
+                            media='min-width: 1100px'/>
                     <img src={this.props.src} alt="pfp station"/>
                 </picture>
             </article>
